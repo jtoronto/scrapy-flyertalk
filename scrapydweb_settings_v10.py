b4f4b22edf86e46d4d92b77740397c14c7e79b0e
Joseph Toronto <toronto.joseph@gmail.com>
2023-11-18 11:08:43 -0500

    added authentication

--- scrapydweb_settings_v10.py
+++ scrapydweb_settings_v10.py
@@ -20,11 +20,10 @@ SCRAPYDWEB_BIND = '0.0.0.0'
 SCRAPYDWEB_PORT = 5000
 
 # The default is False, set it to True to enable basic auth for the web UI.
-ENABLE_AUTH = False
+ENABLE_AUTH = True
 # In order to enable basic auth, both USERNAME and PASSWORD should be non-empty strings.
-USERNAME = ''
-PASSWORD = ''
-
+USERNAME = os.environ.get('USERNAME', '')
+PASSWORD = os.environ.get('PASSWORD', '')
 
 # Make sure that [Scrapyd](https://github.com/scrapy/scrapyd) has been installed
 # and started on all of your hosts.
